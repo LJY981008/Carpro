4dc6242dfa7b3715afc02e9e61d3b475cb08e98c
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2022-06-17 18:49:38 +0900

    기능 완(요청후) 디자인만 넣으면 됨

--- app/src/main/java/com/example/carpro/fragment/MainFragment.kt
+++ app/src/main/java/com/example/carpro/fragment/MainFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager.widget.PagerAdapter
 import com.example.carpro.R
 import com.example.carpro.databinding.FragmentMainBinding
 import com.example.carpro.databinding.FragmentSaleBinding
+import com.example.carpro.topTap.FirstTopFragment
+import com.example.carpro.topTap.PageAdapter
+import com.example.carpro.topTap.SecondTopFragment
+import com.example.carpro.topTap.ThirdTopFragment
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
@@ -19,6 +25,17 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        val adapter = PageAdapter(parentFragmentManager)
+
+        adapter.addFragment(FirstTopFragment(), "1번")
+        adapter.addFragment(SecondTopFragment(), "2번")
+        adapter.addFragment(ThirdTopFragment(), "3번")
+
+        binding.viewPager.adapter = adapter
+        /*binding.tablayout.setupWithViewPager(binding.viewPager)*/
+
+
 
         binding.sale.setOnClickListener {
             Navigation.findNavController(binding.root).navigate(R.id.action_global_saleFragment)
