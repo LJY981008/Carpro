3bbb03b3231485b2f286db23ba0e116ef43f7a63
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2022-06-17 14:46:32 +0900

    원부조회, 사고이력 완

--- app/src/main/java/com/example/carpro/fragment/AccidentFragment.kt
+++ app/src/main/java/com/example/carpro/fragment/AccidentFragment.kt
@@ -5,15 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.carpro.R
+import com.example.carpro.databinding.FragmentAccidentBinding
 
 class AccidentFragment : Fragment() {
-
+    private lateinit var binding: FragmentAccidentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_accident, container, false)
+        binding = FragmentAccidentBinding.inflate(inflater, container, false)
+
+        binding.btnSearch.setOnClickListener {
+            if(binding.editCarNumber.text.isNullOrBlank()) {
+                Toast.makeText(context, "차량번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            }else{
+                binding.tvResult.text = "${binding.editCarNumber.text}" + "의 사고이력"
+            }
+
+        }
+
+
+        return binding.root
     }
 }
--- app/src/main/java/com/example/carpro/fragment/RepairFragment.kt
+++ app/src/main/java/com/example/carpro/fragment/RepairFragment.kt
@@ -5,16 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.carpro.R
+import com.example.carpro.databinding.FragmentRepairBinding
 
 class RepairFragment : Fragment() {
-
+    private lateinit var binding: FragmentRepairBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_repair, container, false)
+        binding = FragmentRepairBinding.inflate(inflater, container, false)
+
+        binding.btnSearch.setOnClickListener {
+            if(binding.editCarNumber.text.isNullOrBlank()) {
+                Toast.makeText(context, "차량번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            }else{
+                binding.tvResult.text = "${binding.editCarNumber.text}" + "의 원부조회 결과"
+            }
+
+        }
+        
+        return binding.root
     }
 
 }
